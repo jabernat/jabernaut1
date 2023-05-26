117dfc287476350723681c2a8d122f6977b27d33
James Neal Abernathy <jabernat@gmail.com>
2023-05-25 21:26:48 -0700

    Added `DriverOpMode` for mechanum tele-op.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOpMode.java
@@ -0,0 +1,186 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+
+
+
+@TeleOp(name="Driver OpMode", group="jabernaut1")
+public class DriverOpMode extends OpMode {
+    private final ElapsedTime runtime = new ElapsedTime();
+    private long loops = 0;
+
+    private DcMotorEx wheelFrontLeft = null;
+    private DcMotorEx wheelFrontRight = null;
+    private DcMotorEx wheelBackLeft = null;
+    private DcMotorEx wheelBackRight = null;
+
+
+
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        final String CAPTION_STATUS = "<strong>Status</strong>";
+        telemetry.setDisplayFormat(Telemetry.DisplayFormat.HTML);
+        telemetry.setCaptionValueSeparator(": ");
+        telemetry.addData(CAPTION_STATUS, "<em>Initializing…</em>");
+        telemetry.update();
+
+
+        // Configure drive wheels
+        final DcMotorEx.Direction directionLeft = DcMotorSimple.Direction.FORWARD;
+        final DcMotorEx.Direction directionRight = DcMotorSimple.Direction.REVERSE;
+
+        wheelFrontLeft = hardwareMap.get(DcMotorEx.class, "WheelFrontLeft");
+        wheelFrontLeft.setDirection(directionLeft);
+
+        wheelFrontRight = hardwareMap.get(DcMotorEx.class, "WheelFrontRight");
+        wheelFrontRight.setDirection(directionRight);
+
+        wheelBackLeft = hardwareMap.get(DcMotorEx.class, "WheelBackLeft");
+        wheelBackLeft.setDirection(directionLeft);
+
+        wheelBackRight = hardwareMap.get(DcMotorEx.class, "WheelBackRight");
+        wheelBackRight.setDirection(directionRight);
+
+
+        // TODO: Enable manual polling
+
+
+        telemetry.addData(CAPTION_STATUS, "<font color=green>Ready.</font>");
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+
+
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+        loops = 0;
+
+        telemetry.setMsTransmissionInterval(100);
+    }
+
+    private static final double DEAD_ZONE_RADIUS = 0.1;
+    private static final double STICK_LINEAR_TO_MOTORPOWER_POWER = 3.0;
+    private static double getPercentOfRange(final double value, final double min, final double max) {
+        final double rangeSize = max - min;
+        return Math.min(1.0, Math.max(0.0,  // Clamp within [0, 1]
+            (value - min) / rangeSize));
+    }
+    private static Vec2D getGamepadStickVector(final float x, final float y)
+    {
+        final Vec2D stick = new Vec2D(x, -y);
+
+        // Normalize and apply dead-zone
+        final double liveZoneMagnitude = getPercentOfRange(
+            stick.getMagnitude(), DEAD_ZONE_RADIUS, 1.0);
+        stick.changeMagnitude(liveZoneMagnitude);
+
+        return stick;
+    }
+    private static Vec2D getGamepadStickLeftVector(final Gamepad gamepad)
+    {
+        return getGamepadStickVector(gamepad.left_stick_x, gamepad.left_stick_y);
+    }
+    private static Vec2D getGamepadStickRightVector(final Gamepad gamepad)
+    {
+        return getGamepadStickVector(gamepad.right_stick_x, gamepad.right_stick_y);
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        telemetry.addData("<strong>Rate</strong>", "<tt>%.1f</tt> Hz", ++loops / runtime.seconds());
+
+
+        // Sample inputs
+        final Vec2D gamepad1StickLeft = getGamepadStickLeftVector(gamepad1);
+        final Vec2D gamepad1StickRight = getGamepadStickRightVector(gamepad1);
+
+        final Vec2D gamepad2StickLeft = getGamepadStickLeftVector(gamepad2);
+        final Vec2D gamepad2StickRight = getGamepadStickRightVector(gamepad2);
+
+
+        final Vec2D driveTranslation = gamepad1StickLeft;
+        double driveRotation = gamepad1StickRight.getX();
+        telemetry.addData("<strong>Drive Translation</strong>", "<em>X</em>=<tt>%3.0f</tt>%%, <em>Y</em>=<tt>%3.0f</tt>%%",
+            driveTranslation.getX() * 100.0, driveTranslation.getY() * 100.0);
+        telemetry.addData("<strong>Drive Rotation</strong>", "<tt>%3.0f</tt>%%",
+            driveRotation * 100.0);
+
+
+        // Apply drive wheel powers
+        {
+            // Convert linear stick percent to power for approximately linear velocity
+            {
+                final double magnitude = driveTranslation.getMagnitude();
+                driveTranslation.changeMagnitude(Math.pow(magnitude, STICK_LINEAR_TO_MOTORPOWER_POWER));
+
+                driveRotation = Math.pow(driveRotation, STICK_LINEAR_TO_MOTORPOWER_POWER);
+            }
+
+
+            double powerFrontLeft  = driveTranslation.getY() + driveTranslation.getX() + driveRotation;
+            double powerFrontRight = driveTranslation.getY() - driveTranslation.getX() - driveRotation;
+            double powerBackLeft   = driveTranslation.getY() - driveTranslation.getX() + driveRotation;
+            double powerBackRight  = driveTranslation.getY() + driveTranslation.getX() - driveRotation;
+
+            // Normalize to a max of 100% power
+            double powerMax = Math.max(
+                Math.max(Math.abs(powerFrontLeft), Math.abs(powerFrontRight)),
+                Math.max(Math.abs(powerBackLeft), Math.abs(powerBackRight)));
+            if (powerMax > 1.0) {
+                powerFrontLeft /= powerMax;
+                powerFrontRight /= powerMax;
+                powerBackLeft /= powerMax;
+                powerBackRight /= powerMax;
+            }
+
+
+            // TODO: Use velocity-mode PID/PIDF.
+            wheelFrontLeft.setPower(powerFrontLeft);
+            wheelFrontRight.setPower(powerFrontRight);
+            wheelBackLeft.setPower(powerBackLeft);
+            wheelBackRight.setPower(powerBackRight);
+
+            telemetry.addData("<strong>Front</strong>", "<em>Left</em>=<tt>%3.0f</tt>%%, <em>Right</em>=<tt>%3.0f</tt>%%",
+                powerFrontLeft * 100.0, powerFrontRight * 100.0);
+            telemetry.addData("<strong>Back</strong>", "<em>Left</em>=<tt>%3.0f</tt>%%, <em>Right</em>=<tt>%3.0f</tt>%%",
+                powerBackLeft * 100.0, powerBackRight * 100.0);
+        }
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+        wheelFrontLeft.setPower(0.0);
+        wheelFrontRight.setPower(0.0);
+        wheelBackLeft.setPower(0.0);
+        wheelBackRight.setPower(0.0);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vec2D.java
@@ -0,0 +1,104 @@
+package org.firstinspires.ftc.teamcode;
+
+
+
+
+public class Vec2D {
+    private double x;
+    private double y;
+
+    public Vec2D() {
+        this(0.0, 0.0);
+    }
+    public Vec2D(final double x, final double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public Vec2D(final Vec2D other) {
+        x = other.x;
+        y = other.y;
+    }
+
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+
+    public double getMagnitude() {
+        return Math.hypot(x, y);
+    }
+    public void changeMagnitude(final double magnitude) {
+        final double magnitudeOld = getMagnitude();
+        if (magnitudeOld == 0.0) {
+            return;
+        }
+
+        // Normalize and apply new magnitude
+        multiply(magnitude / magnitudeOld);
+    }
+
+    public double dotProduct(final Vec2D other) {
+        return x * other.x + y * other.y;
+    }
+
+    public void normalize() {
+        final double magnitude = getMagnitude();
+        if (magnitude == 0.0) {  // Undefined
+            x = Double.NaN;
+            y = Double.NaN;
+        } else {
+            x /= magnitude;
+            y /= magnitude;
+        }
+    }
+    public Vec2D normalized() {
+        final double length = getMagnitude();
+        if (length == 0.0) {  // Undefined
+            return new Vec2D(Double.NaN, Double.NaN);
+        } else {
+            return new Vec2D(x / length, y / length);
+        }
+    }
+
+    public void negate() {
+        x = -x;
+        y = -y;
+    }
+    public Vec2D negated() {
+        return new Vec2D(-x, -y);
+    }
+
+    public void add(final Vec2D other) {
+        x += other.x;
+        y += other.y;
+    }
+    public Vec2D added(final Vec2D other) {
+        return new Vec2D(x + other.x, y + other.y);
+    }
+
+    public void subtract(final Vec2D other) {
+        x -= other.x;
+        y -= other.y;
+    }
+    public Vec2D subtracted(final Vec2D other) {
+        return new Vec2D(x - other.x, y - other.y);
+    }
+
+    public void multiply(final double coefficient) {
+        x *= coefficient;
+        y *= coefficient;
+    }
+    public Vec2D multiplied(final double coefficient) {
+        return new Vec2D(x * coefficient, y * coefficient);
+    }
+
+    public void divide(final double coefficient) {
+        x /= coefficient;
+        y /= coefficient;
+    }
+    public Vec2D divided(final double coefficient) {
+        return new Vec2D(x / coefficient, y / coefficient);
+    }
+}
